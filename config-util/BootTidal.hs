5bf69f076d2368455d0531bc8e9045a344f8906d
IliaViazov <iliaviazov@gmail.com>
2025-12-14 00:35:40 +0100

    ko

--- config-util/BootTidal.hs
+++ config-util/BootTidal.hs
@@ -23,7 +23,7 @@ instance Tidally where tidal = tidalInst
 -- You can also add your own aliases in this file. For example:
 -- fastsquizzed pat = fast 2 $ pat # squiz 1.5
 
-:set prompt "\ESC[95m\STXtidal\ESC[m\STX> "
+:set prompt "\ESC[95m\STXtcii\ESC[m\STX> "
 :set prompt-cont ""
 
 -- custom quit command that kills tmux too
